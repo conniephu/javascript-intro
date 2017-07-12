5366da9dd1745ef1dc133201fa56b6d34eb6eaf6
conniephu <connie.phu@C02NG0WLG3QR.local>
2017-07-11 20:17:50 -0500

    updating file from class

---
+++ Scripts/app.js
@@ -0,0 +1,11 @@
+window.onload = function(event) {
+	console.log('Hi mom');
+	console.log(document);
+	let ul = document.createElement('ul');
+	console.log(ul);
+
+
+
+}
+
+document.getElementByID('list').innerHTML = 'I like cats';
